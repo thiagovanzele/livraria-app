5ffb4fb7b01876719626a9e8b7d767ecd9673120
Thiago Vanzele <thiagovanzele@gmail.com>
2024-07-12 16:47:09 -0300

    Entidade livro criada

---
+++ src/main/java/br/com/teste/livraria/model/entities/Resumo.java
@@ -0,0 +1,27 @@
+package br.com.teste.livraria.model.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+@Entity
+@Table(name = "tb_livro")
+public class Resumo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String title;
+}
