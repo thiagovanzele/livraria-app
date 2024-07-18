b07f47c48e38a67a0be44399826595d8c61479ab
Thiago Vanzele <thiagovanzele@gmail.com>
2024-07-18 16:33:02 -0300

    Metodos Get, Delete e Put de autor

--- src/main/java/br/com/teste/livraria/controllers/AutorController.java
+++ src/main/java/br/com/teste/livraria/controllers/AutorController.java
@@ -1,10 +1,14 @@
 package br.com.teste.livraria.controllers;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +25,18 @@ public class AutorController {
 	@Autowired
 	private AutorService service;
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<Autor> findById(@PathVariable Long id) {
+		Autor autor = service.findById(id);
+		return ResponseEntity.ok(autor);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Autor>> findAll() {
+		List<Autor> list = service.findAll();
+		return ResponseEntity.ok(list);
+	}
+		
 	@PostMapping
 	public ResponseEntity<Autor> insert(@RequestBody AutorDto autorDto) {
 		Autor autor = service.insert(autorDto);
@@ -30,5 +46,11 @@ public class AutorController {
 				.buildAndExpand(autor.getId()).toUri();
 		
 		return ResponseEntity.created(uri).body(autor);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Autor> update(@PathVariable Long id, @RequestBody AutorDto autorDto) {
+		Autor autor = service.update(id, autorDto);
+		return ResponseEntity.ok(autor);
 	}
 }
--- src/main/java/br/com/teste/livraria/model/services/AutorService.java
+++ src/main/java/br/com/teste/livraria/model/services/AutorService.java
@@ -1,6 +1,6 @@
 package br.com.teste.livraria.model.services;
 
-import java.util.stream.Collectors;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import br.com.teste.livraria.model.dtos.AutorDto;
 import br.com.teste.livraria.model.entities.Autor;
 import br.com.teste.livraria.model.entities.Livro;
+import br.com.teste.livraria.model.exceptions.ResourceNotFoundException;
 import br.com.teste.livraria.model.repositories.AutorRepository;
 import br.com.teste.livraria.model.repositories.LivroRepository;
 import jakarta.transaction.Transactional;
@@ -17,20 +18,60 @@ public class AutorService {
 
 	@Autowired
 	private AutorRepository autorRepository;
-	
+
 	@Autowired
 	private LivroRepository livroRepository;
-	
+
+	public Autor findById(Long id) {
+		return autorRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+
+	public List<Autor> findAll() {
+		return autorRepository.findAll();
+	}
+
 	@Transactional
 	public Autor insert(AutorDto autorDto) {
 		Autor autor = new Autor(autorDto, livroRepository);
-			
+
 		for (Livro livro : autor.getLivros()) {
 			if (!livro.getAutores().contains(autor)) {
 				livro.getAutores().add(autor);
 			}
 		}
+
+		return autorRepository.save(autor);
+	}
+
+	@Transactional
+	public void delete(Long id) {
+		autorRepository.deleteById(id);
+		;
+	}
+
+	@Transactional
+	public Autor update(Long id, AutorDto autorDto) {
+		
+		Autor autor = autorRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
+		
+		updateData(autor, autorDto);
 		
 		return autorRepository.save(autor);
+		
+	}
+
+	private void updateData(Autor autor, AutorDto autorDto) {
+		autor.setNome(autorDto.nome());
+		
+		autorDto.livrosIds().stream().forEach(idLivro -> {
+			Livro livro = livroRepository.findById(idLivro).orElseThrow(() -> new ResourceNotFoundException(idLivro));
+			if (!autor.getLivros().contains(livro)) {
+				autor.getLivros().add(livro);
+			}
+			
+			if(!livro.getAutores().contains(autor)) {
+				livro.getAutores().add(autor);
+			}
+		});
 	}
 }
