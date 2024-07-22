db27632c53ec143fd2e50d1c7284e569f6ec8282
Thiago Vanzele <thiagovanzele@gmail.com>
2024-07-22 17:11:10 -0300

    Crud de Cliente

--- src/main/java/br/com/teste/livraria/controllers/ClienteController.java
+++ src/main/java/br/com/teste/livraria/controllers/ClienteController.java
@@ -1,10 +1,15 @@
 package br.com.teste.livraria.controllers;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +25,31 @@ public class ClienteController {
 
 	@Autowired
 	private ClienteService service;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> findById(@PathVariable Long id) {
+		Cliente cliente = service.findById(id);
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Cliente>> findAll() {
+		List<Cliente> clientes = service.findAll();
+		return ResponseEntity.ok().body(clientes);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Cliente> update(@PathVariable Long id, @RequestBody ClienteDto clienteDto) {
+		Cliente cliente = service.update(id, clienteDto);
+		return ResponseEntity.ok().body(cliente);
+	}
+	
 	
 	@PostMapping
 	public ResponseEntity<Cliente> insert (@RequestBody ClienteDto clienteDto) {
--- src/main/java/br/com/teste/livraria/model/services/ClienteService.java
+++ src/main/java/br/com/teste/livraria/model/services/ClienteService.java
@@ -1,11 +1,14 @@
 package br.com.teste.livraria.model.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.teste.livraria.model.dtos.ClienteDto;
 import br.com.teste.livraria.model.entities.Cliente;
 import br.com.teste.livraria.model.entities.Endereco;
+import br.com.teste.livraria.model.exceptions.ResourceNotFoundException;
 import br.com.teste.livraria.model.repositories.ClienteRepository;
 
 @Service
@@ -27,5 +30,37 @@ public class ClienteService {
 		cliente.setEndereço(endereco);
 
 		return clienteRepository.save(cliente);
+	}
+
+	public Cliente findById(Long id) {
+		return clienteRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(Cliente.class, id));
+	}
+
+	public List<Cliente> findAll() {
+		List<Cliente> list = clienteRepository.findAll();
+		return list;
+	}
+
+	public void delete(Long id) {
+		if (clienteRepository.existsById(id)) {
+			clienteRepository.deleteById(id);
+		}
+
+		throw new ResourceNotFoundException(Cliente.class, id);
+	}
+	
+	public Cliente update(Long id, ClienteDto clienteDto) {
+		Cliente cliente = clienteRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(Cliente.class, id));
+		updateData(cliente, clienteDto);
+		return clienteRepository.save(cliente);
+		
+	}
+
+	private void updateData(Cliente cliente, ClienteDto clienteDto) {
+		Endereco endereco = enderecoService.buscarEnderecoPorCep(clienteDto.cep(), clienteDto.numero());
+		cliente.setEndereço(endereco);
+		cliente.setEmail(clienteDto.email());
+		cliente.setNome(clienteDto.nome());
+		
 	}
 }
