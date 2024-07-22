747e0a2b8f3858ac44d150b5ce576996b751efe0
Thiago Vanzele <thiagovanzele@gmail.com>
2024-07-22 15:42:03 -0300

    ValidationExceptionHandler

--- src/main/java/br/com/teste/livraria/controllers/exceptions/ControllerExceptionHandler.java
+++ src/main/java/br/com/teste/livraria/controllers/exceptions/ControllerExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import br.com.teste.livraria.model.exceptions.ResourceNotFoundException;
+import br.com.teste.livraria.model.exceptions.ValidationException;
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
@@ -18,6 +19,15 @@ public class ControllerExceptionHandler {
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(ValidationException.class)
+	public ResponseEntity<StandardError> validation(ValidationException e, HttpServletRequest request) {
+		String error = "Valor não pode ser nulo";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		
 		return ResponseEntity.status(status).body(err);
 	}
 }
